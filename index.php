c70a1769c670c28828a61dab999c8305947f9e95
unknown <edouardnoel.tardif@gmail.com>
2020-03-24 18:42:38 +0100

    Changement path pour le

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <pre><?php var_dump($_POST)?></pre>
+        <pre><?php var_dump($_GET)?></pre>
+        <pre><?php var_dump($_SERVER)?></pre>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
